94801506d3ebbe2568b92fad2b0f9082743f63d5
DHEERAJ MAURYA <46066156+jareehd@users.noreply.github.com>
2019-11-10 18:49:23 +0530

    menu bar

--- index1/index1.js
+++ index1/index1.js
@@ -1,1 +1,24 @@
 
+$(function() {
+    AOS.init();
+  });
+  $(window).on('load', function() {
+    AOS.refresh();
+  });
+
+$(document).ready(function(){
+    $("#close-button").click(function(){        
+            $('#myNavbar').removeClass('show');
+            $('#myNavbar').addClass('hide');
+            
+            $('#menu').removeClass('hide');
+            $('#menu').addClass('show');    
+    });
+    
+    $("#menu").click(function(){
+        $('#menu').removeClass('show');
+        $('#menu').addClass('hide');
+        $('#myNavbar').removeClass('hide');
+        $('#myNavbar').addClass('show');   
+   });
+});
